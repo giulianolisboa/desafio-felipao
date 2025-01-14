e16434b5bcce39e4eaae434b015fff7046a323af
giulianolisboa <giuliano.lisboa@icloud.com>
2025-01-13 21:51:19 -0400

    Desafio do Héroi

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-let teste = "Testanto Atualização"
+//Desafio nével do héroi
+let nomeDoHeroi = "João do Agreste";
+let xp = 8435;
 
-console.log("Certo")
+let nivel = "";
+
+if (xp < 1000){
+    nivel = "Ferro";
+}else if (xp >= 1001 && <= 2000){
+    nivel = "Bronze";
+}else if (xp >= 2001 && <= 5000){
+    nivel = "Prata";
+}else if (xp >= 5001 && <= 7000){
+    nivel = "Ouro";
+}else if (xp >= 7001 && <= 8000){
+    nivel = "Platina";
+}else if (xp >= 8001 && <= 9000){
+    nivel = "Ascedente";
+}else if (xp >= 9001 && <= 10000){
+    nivel = "Imortal";
+}else if (xp >= 10001){
+    nivel = "Radiante";
+}
+//Saída
+
+console.log("O hério de nome "  nomeDoHeroi " está no nível de " nivel)
 
